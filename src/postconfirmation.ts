f050929d46603378e717e765c539acc9fdc41221
Kartik Rao <kartik.rao@globalred.com.au>
2019-03-12 17:53:38 +1100

    Updated post confirmation trigger and logic, added Cognito schema

--- src/postconfirmation.ts
+++ src/postconfirmation.ts
@@ -1,97 +1,157 @@
-//# sourceMappingURL=./append.js.map
+//# sourceMappingURL=./postconfirmation.js.map
 require('source-map-support').install();
 process.env.TZ = 'UTC';
 
-import {Logger, SEVERITY} from "@adinfinity/ai-lambda-logging";
-import {APIGatewayEventRequestContext, APIGatewayEvent} from 'aws-lambda';
+import {CognitoUserPoolTriggerEvent} from 'aws-lambda';
 import * as _ from "lodash";
 import {DBClient} from "./dbclient";
 const uuid = require('uuid/v5');
 import * as AWS from 'aws-sdk';
+import { AdminGetUserRequest } from "aws-sdk/clients/cognitoidentityserviceprovider";
 
 
-const ServiceName = process.env['serviceName'] || 'dev-formsgraphql-postconfirmation';
-const DDB_TABLE : string = process.env.TBL_MASTER_DATA || 'formsgraphql_dev_masterdata';
+const ServiceName = process.env.serviceName || 'dev-formsgraphql-postconfirmation';
+const DDB_TABLE : string = process.env.table_app_data || 'formsgraphql_dev_masterdata';
 
-export const handle = (event : APIGatewayEvent, context : APIGatewayEventRequestContext, callback : any) => {
+export const handle = async (event : CognitoUserPoolTriggerEvent, context : any, callback : any) => {
+    console.log(`${ServiceName} - postconfirmation.handle`, event);
 
-    const logger = new Logger(ServiceName, context.stage, SEVERITY.INFO);
-    logger.setAPIGContext(event.requestContext);
-
-    const cognitoReq : any = event["request"];
+    const cognitoReq = event.request;
     const {userAttributes} = cognitoReq;
-    const accountId = uuid(`${cognitoReq.userPoolId}.amazonaws.com`, uuid.DNS);
+
     const ddbClient = DBClient.getInstance({convertEmptyValues: true});
 
-    // 1. Create Account with name as tenant + Create user with id as cognito sub
-    ddbClient.batchWrite({
-        RequestItems: {
-            [DDB_TABLE] : [
-                {
-                    PutRequest : {
-                        Item: {
-                            partitionKey: accountId,
-                            sortKey: 'ACCOUNT',
-                            indexKey: 'FREE',
-                            itemData: {
-                                version: 1,
-                                owner  : userAttributes.sub,
-                                name   : userAttributes["custom:tenant"]
-                            }
-                        }
-                    }
-                },
-                {
-                    PutRequest : {
-                        Item: {
-                            partitionKey: userAttributes.sub,
-                            sortKey : 'USER',
-                            indexKey: `${accountId}:${userAttributes.sub}:Admin`,
-                            itemData: {
-                                accountId : accountId,
-                                group: 'AccountAdmin',
-                                ...userAttributes
-                            }
-                        }
-                    }
+    let source = userAttributes["custom:source"];
+    let userId = userAttributes.sub;
+    let userPool = new AWS.CognitoIdentityServiceProvider();
+
+    if (!source) {
+        // New Account Sign up
+        // 1. Create Account with name as tenant + Create user with id as cognito sub
+        let tenantName = userAttributes["custom:tenantName"];
+        let accountId = uuid();
+        let group = 'AccountAdmin';
+        console.log(`${ServiceName} - New Account Flow - Init name=[${tenantName}] gen_id=[${accountId}]`);
+        console.log(`${ServiceName} - New Account Flow - ddb.Put Account`);
+        await ddbClient.put({
+            TableName: DDB_TABLE,
+            Item: {
+                id   : accountId,
+                type : "ACCOUNT",
+                itemType : "ACCOUNT",
+                owner: userId,
+                name : tenantName,
+                createdAt : new Date().toISOString()
+            }
+        }).promise();
+
+        console.log(`${ServiceName} - New Account Flow - ddb.Put User`);
+        await ddbClient.put({
+            TableName: DDB_TABLE,
+            Item: {
+                id   : userId,
+                type : "USER",
+                meta : `${accountId}#${group}`,
+                itemType : "USER",
+                owner: userId,
+                group: group,
+                accountId: accountId,
+                email: userAttributes["email"],
+                phone_number: userAttributes["phone_number"],
+                given_name: userAttributes["given_name"],
+                family_name: userAttributes["family_name"],
+                createdAt : new Date().toISOString()
+        }}).promise();
+
+        // 2. Set Cognito Group to Account Admin
+        console.log(`${ServiceName} - New Account Flow - adminAddUserToGroup [AccountAdmin]`);
+        let addUserToGroupParams = {
+            GroupName : 'AccountAdmin',
+            UserPoolId: event.userPoolId,
+            Username  :  event.userName
+        };
+        await userPool.adminAddUserToGroup(addUserToGroupParams).promise();
+
+        // 3. Update user attributes
+        console.log(`${ServiceName} - New Account Flow - adminUpdateUserAttributes`);
+        let updateUserAttributesParams = {
+            UserAttributes: [
+                {Name: "custom:region", Value: process.env.region},
+                {Name: "custom:environment", Value: process.env.environment},
+                {Name: "custom:tenantName",  Value: tenantName },
+                {Name: "custom:tenantId",  Value: accountId }
+            ],
+            UserPoolId: event.userPoolId,
+            Username: event.userName
+        };
+        await userPool.adminUpdateUserAttributes(updateUserAttributesParams).promise();
+        callback(null, event);
+    } else {
+        // AccountAdmin initiated signup
+        let group = userAttributes["custom:group"];
+        if (group == 'Admin') {
+            // Do not allow Admin as a group
+            group = 'AccountAdmin';
+        }
+        // Owner's sub is in the source
+        console.log(`${ServiceName} - AccountAdmin initiated flow - Init userId=[${userId}] group=[${group}]`);
+        let accountAdmin: AWS.CognitoIdentityServiceProvider.AdminGetUserResponse;
+        try {
+            console.log(`${ServiceName} - AccountAdmin initiated flow - ${source}`);
+            accountAdmin = await userPool.adminGetUser(<AdminGetUserRequest>{
+                UserPoolId: event.userPoolId, Username: source
+            }).promise();
+
+            let accountId = accountAdmin.UserAttributes["custom:tenantId"];
+            if(!accountId) {
+                let err = new Error("InvalidUserCreateRequest")
+                console.error(`${ServiceName} - Invalid user, no tenant on AccountAdmin`, err);
+                throw err;
+            }
+
+            console.log(`${ServiceName} - AccountAdmin initiated flow - ddb.Put user=[${userId}] tenant=[${accountId}] group=[${group}]`);
+            await ddbClient.put({
+                TableName: DDB_TABLE,
+                Item: {
+                    id   : userId,
+                    type : "USER",
+                    meta : `${accountId}#${group}`,
+                    itemType : "USER",
+                    owner: source,
+                    group: group,
+                    accountId: accountId,
+                    email: userAttributes["email"],
+                    phone_number: userAttributes["phone_number"],
+                    given_name: userAttributes["given_name"],
+                    family_name: userAttributes["family_name"],
+                    createdAt : new Date().toISOString()
                 }
-            ]
-        }
-    }, (err, data) => {
-        if(err) {
-            callback(err);
-        } else {
-            // Account and User created successfully in DDB
-            let cognitoParams = {
-                GroupName : 'Admin',
-                UserPoolId: cognitoReq.userPoolId, /* required */
-                Username  :  event["request"]["userName"]/* required */
+            }).promise();
+
+            let cognitoSetGroupParams : AWS.CognitoIdentityServiceProvider.Types.AdminAddUserToGroupRequest = {
+                GroupName : group,
+                UserPoolId: event.userPoolId,
+                Username  : event.userName
             };
-            // 2. Update the tenant (which had the name) with the account id
-            var userPool = new AWS.CognitoIdentityServiceProvider();
-            userPool.adminAddUserToGroup(cognitoParams, (err, data) => {
-                let params = {
-                    UserAttributes: [ /* required */
-                      {
-                        Name: 'custom:tenant', /* required */
-                        Value: accountId
-                      }
-                    ],
-                    UserPoolId: cognitoReq.userPoolId, /* required */
-                    Username: cognitoReq.userName /* required */
-                };
-                // 3. Add User to 'Admin'
-                userPool.adminUpdateUserAttributes(params, (err, data) => {
-                    if(err) {
-                        console.log(err);
-                        callback(err);
-                    } else {
-                        callback(null, {statusCode: 200});
-                    }
-                })
-            });
+
+            console.log(`${ServiceName} - AccountAdmin initiated flow - adminAddUserToGroup username=[${event.userName}] group=[${group}]`);
+            await userPool.adminAddUserToGroup(cognitoSetGroupParams).promise();
+            let cognitoUpdateAttributesParams : AWS.CognitoIdentityServiceProvider.Types.AdminUpdateUserAttributesRequest= {
+                UserPoolId: event.userPoolId,
+                Username  : event.userName,
+                UserAttributes : [
+                    {Name: "custom:region", Value: process.env.region},
+                    {Name: "custom:environment", Value: process.env.environment},
+                    {Name: "custom:tenant", Value: accountId},
+                ]
+            };
+            await userPool.adminUpdateUserAttributes(cognitoUpdateAttributesParams).promise();
+            callback(null, event);
+        } catch (error) {
+            console.error(`${ServiceName} - AccountAdmin initiated flow - Error [${userId}]`, error);
+            callback(error)
         }
-    })
+    }
 }
 
 /**
---
+++ test/spec.auth.ts
@@ -0,0 +1,39 @@
+require('isomorphic-fetch');
+import Auth from '@aws-amplify/auth';
+import Amplify from '@aws-amplify/core';
+const config = require("../outputs/stack.json");
+
+var crypto = require('crypto');
+
+describe("Cognito", () => {
+    beforeAll( async () => {
+      Amplify.configure({
+        Auth: {
+          region: 'ap-southeast-2',
+          userPoolId: config["UserPoolId"],
+          userPoolWebClientId: config["UserPoolClientId"]
+        }
+      });
+
+    })
+
+    afterAll(async (done) => {
+        await Auth.signOut();
+        done();
+    })
+
+    it("can sign in as Admin user", async () => {
+        const user = await Auth.signIn(config["UserPoolAdminUser"], "2fb#owbV");
+        expect(user).toBeDefined();
+    });
+
+    it("can setup a new account", async () => {
+        let prefix = `${Math.random() * 1e6}`.split(".")[0]
+        let email = `${prefix}@mailinator.com`
+        const response = await Auth.signUp({
+            username: email,
+            password: `P@ssword1`
+        });
+
+    })
+});
