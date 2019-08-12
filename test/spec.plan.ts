8fdf4f80a0b5e76c21cd6b003225e396c84c43dd
Kartik Rao <kartik.rao@adinfinity.com>
2019-08-12 19:04:28 +1000

    Bugfixes

--- test/spec.plan.ts
+++ test/spec.plan.ts
@@ -142,7 +142,7 @@ describe("Plan", () => {
             expect(hasErrors).toBeFalsy("Response should not have errors");
             hasErrors && done.fail(errors[0].message);
             expect(parsed).toBeDefined("Response.data should exist");
-            expect(parsed.active).toBeTruthy();
+            expect(parsed.active).toBeFalsy();
             expect(parsed.version).toBeGreaterThan(planVersion);
             planVersion = parsed.version;
         } catch (error) {
