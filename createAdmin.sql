ec88df64d511e07ecda469e734f6d892c4a57d5c
Kartik Rao <kartik.rao@adinfinity.com>
2019-08-15 00:36:52 +1000

    Admin SQL

---
+++ createAdmin.sql
@@ -0,0 +1,7 @@
+insert into User(id, email, userGroup, given_name, family_name, createdAt)
+    values("2c0a5735-4204-4349-a8b9-d83239437d2b",
+	"infrastructure.dev@forms.li",
+    "Admin",
+    "Kartik",
+    "Rao",
+    "2019-08-14T12:38:42.064Z")
