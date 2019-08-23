3225a1873f8f9f6c6c63c609f19da3c2edf9dc92
Kartik Rao <kartik.rao@adinfinity.com>
2019-08-23 18:53:45 +1000

    Add init sql script

---
+++ src/sql/init.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+User (id, given_name, family_name, email, userGroup, createdAt)
+VALUES (
+    '270eb5e3-657f-48ed-93f1-1066e299f52c',
+    'Kartik',
+    'Rao',
+    'infrastructure.dev@forms.li',
+    'Admin',
+    '2019-08-23T08:34:48.287Z');
