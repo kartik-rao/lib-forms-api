857e371247e6d8590f0f1529853976043e8a7de3
Kartik Rao <kartik.rao@adinfinity.com>
2019-09-18 20:45:43 +1000

    Refactor to work with SQS

--- src/processentries.ts
+++ src/processentries.ts
@@ -2,7 +2,7 @@
 require('source-map-support').install();
 process.env.TZ = 'UTC';
 
-import {APIGatewayEventRequestContext, APIGatewayEvent} from 'aws-lambda';
+import {APIGatewayEventRequestContext, SQSEvent, SQSRecord} from 'aws-lambda';
 import * as AWS from "aws-sdk";
 
 const Region   = process.env.region;
@@ -15,26 +15,20 @@ const CORS_HEADERS = {
     "Content-Type": "application/json"
 }
 
-export const handle = async (event : APIGatewayEvent, context : APIGatewayEventRequestContext, callback : any) => {
+export const handle = async (event : SQSEvent, context : APIGatewayEventRequestContext, callback : any) => {
     let entryQueue = new AWS.SQS({region: Region});
     let firehose = new AWS.Firehose({region: Region});
     try {
-        let messages = await entryQueue.receiveMessage({
-            VisibilityTimeout: 120, 
-            MaxNumberOfMessages:10, 
-            QueueUrl: QueueUrl, 
-            AttributeNames : ["StreamName"] }).promise();
-
-        messages.Messages.forEach(async (message) => {
-            let {StreamName} = message.Attributes;
+        event.Records.forEach(async (message: SQSRecord) => {
+            let StreamName = message.messageAttributes["StreamName"];
             try {
-                await firehose.putRecord({DeliveryStreamName: StreamName, Record: {Data : message.Body}}).promise();
-                await entryQueue.deleteMessage({QueueUrl: QueueUrl, ReceiptHandle: message.ReceiptHandle}).promise();
+                await firehose.putRecord({DeliveryStreamName: StreamName.stringValue, Record: {Data : message.body + "/n"}}).promise();
+                await entryQueue.deleteMessage({QueueUrl: QueueUrl, ReceiptHandle: message.receiptHandle}).promise();
                 callback(null, {statusCode: 200, body: JSON.stringify({status: "OK"}), headers: CORS_HEADERS})
             } catch (error) {
                 console.log(`${ServiceName} - recordprocessor.handle message ERROR`, error);
             }
-        })
+        });
     } catch (error) {
         console.log(`${ServiceName} - recordprocessor.handle queue ERROR`, error);
     }
