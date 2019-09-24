4e3ba5bc8c44418bf4d5a92a96bedf820d6b7cca
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-24 14:00:36 +1000

    Bugfix - process.env is undefined - Target should be node

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    target: 'web',
+    target: 'node',
     mode: process.env.NODE_ENV || 'development',
     entry: slsw.lib.entries,
     externals: [nodeExternals()],
