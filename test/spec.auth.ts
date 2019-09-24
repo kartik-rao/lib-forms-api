c2315a06e7ac24422389d151869733f6991dd8fb
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-24 15:16:37 +1000

    Disable tenant test cases, covered in spec.signup

--- test/spec.auth.ts
+++ test/spec.auth.ts
@@ -65,205 +65,205 @@ describe("Auth", () => {
         })
     });
 
-    describe("New Tenant", () => {
-        let verificationCode: string;
-        beforeAll(async (done) => {
-            try {
-                accAdminInbox = await mailSlurp.createInbox();
-                done();
-            } catch (error) {
-                console.error(error);
-                fail(error);
-            }
-        });
+    // describe("New Tenant", () => {
+    //     let verificationCode: string;
+    //     beforeAll(async (done) => {
+    //         try {
+    //             accAdminInbox = await mailSlurp.createInbox();
+    //             done();
+    //         } catch (error) {
+    //             console.error(error);
+    //             fail(error);
+    //         }
+    //     });
 
-        it("Signup", async (done) => {
-            try {
-                accAdmin = await Auth.signUp({
-                    username: accAdminInbox.emailAddress,
-                    password: TestTenantPassword,
-                    attributes : {
-                        "custom:tenantName" : TestTenantName,
-                        "given_name" : "AccountAdmin",
-                        "family_name": TestTenantName
-                    }
-                });
-                expect(accAdmin).toBeDefined();
-                done();
-            } catch (error) {
-                console.error(error);
-                fail(error)
-            }
-        });
+    //     it("Signup", async (done) => {
+    //         try {
+    //             accAdmin = await Auth.signUp({
+    //                 username: accAdminInbox.emailAddress,
+    //                 password: TestTenantPassword,
+    //                 attributes : {
+    //                     "custom:tenantName" : TestTenantName,
+    //                     "given_name" : "AccountAdmin",
+    //                     "family_name": TestTenantName
+    //                 }
+    //             });
+    //             expect(accAdmin).toBeDefined();
+    //             done();
+    //         } catch (error) {
+    //             console.error(error);
+    //             fail(error)
+    //         }
+    //     });
 
-        it("Email Verification", async (done) => {
-            try {
-                const emails = await mailSlurp.getEmails(accAdminInbox.id, { minCount: 1 })
+    //     it("Email Verification", async (done) => {
+    //         try {
+    //             const emails = await mailSlurp.getEmails(accAdminInbox.id, { minCount: 1 })
 
-                expect(emails.length).toBeGreaterThan(0);
-                let email = await mailSlurp.getEmail(emails[0].id);
+    //             expect(emails.length).toBeGreaterThan(0);
+    //             let email = await mailSlurp.getEmail(emails[0].id);
 
-                expect(email).toBeDefined();
-                expect(email.body.length).toBeGreaterThan(1);
-                verificationCode = email.body;
-                done();
-            } catch (error) {
-                console.error(error);
-                fail(error);
-            }
-        });
+    //             expect(email).toBeDefined();
+    //             expect(email.body.length).toBeGreaterThan(1);
+    //             verificationCode = email.body;
+    //             done();
+    //         } catch (error) {
+    //             console.error(error);
+    //             fail(error);
+    //         }
+    //     });
 
-        it("Confirm Sign Up", async (done) => {
-            // Confirm user
-            await Auth.confirmSignUp(accAdminInbox.emailAddress, verificationCode, {
-                forceAliasCreation: true
-            });
-            done();
-        });
+    //     it("Confirm Sign Up", async (done) => {
+    //         // Confirm user
+    //         await Auth.confirmSignUp(accAdminInbox.emailAddress, verificationCode, {
+    //             forceAliasCreation: true
+    //         });
+    //         done();
+    //     });
 
-        it("Sign In", async (done) => {
+    //     it("Sign In", async (done) => {
 
-            await Auth.signIn(accAdminInbox.emailAddress, TestTenantPassword);
-            let user: CognitoUser = await Auth.currentAuthenticatedUser();
+    //         await Auth.signIn(accAdminInbox.emailAddress, TestTenantPassword);
+    //         let user: CognitoUser = await Auth.currentAuthenticatedUser();
 
-            user.getUserAttributes((err, attributes=[]) => {
-                if(err) {
-                    console.error(err);
-                    fail(err);
-                    return;
-                }
+    //         user.getUserAttributes((err, attributes=[]) => {
+    //             if(err) {
+    //                 console.error(err);
+    //                 fail(err);
+    //                 return;
+    //             }
 
-                let attrs = {}
-                attributes.forEach((attr) => {
-                    attrs[attr["Name"]] = attr["Value"];
-                });
+    //             let attrs = {}
+    //             attributes.forEach((attr) => {
+    //                 attrs[attr["Name"]] = attr["Value"];
+    //             });
 
-                expect(attrs['custom:environment']).toBe('dev', "custom:environment should be 'dev' - Check PostConfirmationTrigger");
-                expect(attrs['custom:group']).toBe('AccountAdmin', "custom:group should be 'AccountAdmin'  - Check PostConfirmationTrigger");
-                expect(attrs['custom:tenantName']).toEqual(TestTenantName);
-                expect(attrs['custom:tenantId']).toBeDefined("custom:tenantId should be set  - Check PostConfirmationTrigger");
-                expect(attrs['email_verified']).toBeTruthy();
-                tenantId = attrs['custom:tenantId'];
-                done();
-            });
+    //             expect(attrs['custom:environment']).toBe('dev', "custom:environment should be 'dev' - Check PostConfirmationTrigger");
+    //             expect(attrs['custom:group']).toBe('AccountAdmin', "custom:group should be 'AccountAdmin'  - Check PostConfirmationTrigger");
+    //             expect(attrs['custom:tenantName']).toEqual(TestTenantName);
+    //             expect(attrs['custom:tenantId']).toBeDefined("custom:tenantId should be set  - Check PostConfirmationTrigger");
+    //             expect(attrs['email_verified']).toBeTruthy();
+    //             tenantId = attrs['custom:tenantId'];
+    //             done();
+    //         });
 
-        });
-    });
+    //     });
+    // });
 
-    describe("Existing Tenant", () => {
-        let accViewerInbox: Inbox;
-        let accViewerSub: string;
+    // describe("Existing Tenant", () => {
+    //     let accViewerInbox: Inbox;
+    //     let accViewerSub: string;
 
-        beforeAll(async (done) => {
-            await Auth.signOut();
-            accViewerInbox = await mailSlurp.createInbox();
-            done();
-        });
+    //     beforeAll(async (done) => {
+    //         await Auth.signOut();
+    //         accViewerInbox = await mailSlurp.createInbox();
+    //         done();
+    //     });
 
-        it("Invite User", async (done) => {
-            try {
-                await Auth.signIn(accAdminInbox.emailAddress, TestTenantPassword);
-                let admin: CognitoUser = await Auth.currentAuthenticatedUser();
-                let session: CognitoUserSession = await Auth.currentSession();
-                let token = session.getIdToken().getJwtToken();
+    //     it("Invite User", async (done) => {
+    //         try {
+    //             await Auth.signIn(accAdminInbox.emailAddress, TestTenantPassword);
+    //             let admin: CognitoUser = await Auth.currentAuthenticatedUser();
+    //             let session: CognitoUserSession = await Auth.currentSession();
+    //             let token = session.getIdToken().getJwtToken();
 
-                let invitePayload = {
-                    "custom:group" : "AccountViewer",
-                    "custom:source": admin.getUsername(), // this is sub not email
-                    family_name: TestTenantName,
-                    given_name:"Viewer",
-                    email: accViewerInbox.emailAddress
-                }
+    //             let invitePayload = {
+    //                 "custom:group" : "AccountViewer",
+    //                 "custom:source": admin.getUsername(), // this is sub not email
+    //                 family_name: TestTenantName,
+    //                 given_name:"Viewer",
+    //                 email: accViewerInbox.emailAddress
+    //             }
 
-                let inviteResponse = await fetch(`${config.ServiceEndpoint}/invite`, {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': token,
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(invitePayload)
-                });
+    //             let inviteResponse = await fetch(`${config.ServiceEndpoint}/invite`, {
+    //                 method: 'POST',
+    //                 headers: {
+    //                     'Authorization': token,
+    //                     'Content-Type': 'application/json'
+    //                 },
+    //                 body: JSON.stringify(invitePayload)
+    //             });
 
-                if(inviteResponse.status != 200) {
-                    done.fail(inviteResponse.statusText);
-                    done();
-                    return;
-                }
+    //             if(inviteResponse.status != 200) {
+    //                 done.fail(inviteResponse.statusText);
+    //                 done();
+    //                 return;
+    //             }
 
-                let res: AdminCreateUserResponse = await inviteResponse.json();
-                expect(res).toBeDefined();
-                expect(res.User).toBeDefined();
+    //             let res: AdminCreateUserResponse = await inviteResponse.json();
+    //             expect(res).toBeDefined();
+    //             expect(res.User).toBeDefined();
 
-                accViewerSub = res.User.Username;
-                let attrs = {}
-                res.User.Attributes.forEach((attr) => {
-                    attrs[attr["Name"]] = attr["Value"];
-                });
+    //             accViewerSub = res.User.Username;
+    //             let attrs = {}
+    //             res.User.Attributes.forEach((attr) => {
+    //                 attrs[attr["Name"]] = attr["Value"];
+    //             });
 
-                expect(attrs['custom:environment']).toBe('dev');
-                expect(attrs['custom:group']).toBe('AccountViewer');
-                expect(attrs['custom:tenantId']).toEqual(tenantId);
-                expect(attrs['custom:source']).toEqual(accAdmin.userSub);
-                expect(attrs['custom:tenantName']).toEqual(TestTenantName);
-                done();
+    //             expect(attrs['custom:environment']).toBe('dev');
+    //             expect(attrs['custom:group']).toBe('AccountViewer');
+    //             expect(attrs['custom:tenantId']).toEqual(tenantId);
+    //             expect(attrs['custom:source']).toEqual(accAdmin.userSub);
+    //             expect(attrs['custom:tenantName']).toEqual(TestTenantName);
+    //             done();
 
-            } catch (error) {
-                console.error(error);
-                fail(error);
-            }
-        }, 15000);
+    //         } catch (error) {
+    //             console.error(error);
+    //             fail(error);
+    //         }
+    //     }, 15000);
 
-        it("User login details", async () =>{
-            const emails = await mailSlurp.getEmails(accViewerInbox.id, { minCount: 1 });
-            expect(emails.length).toBeGreaterThan(0);
-            let email = await mailSlurp.getEmail(emails[0].id);
-            expect(email).toBeDefined();
-            expect(email.body.length).toBeGreaterThan(1);
-            expect(email.body.indexOf("temporary password is")).toBeGreaterThan(-1);
-        });
+    //     it("User login details", async () =>{
+    //         const emails = await mailSlurp.getEmails(accViewerInbox.id, { minCount: 1 });
+    //         expect(emails.length).toBeGreaterThan(0);
+    //         let email = await mailSlurp.getEmail(emails[0].id);
+    //         expect(email).toBeDefined();
+    //         expect(email.body.length).toBeGreaterThan(1);
+    //         expect(email.body.indexOf("temporary password is")).toBeGreaterThan(-1);
+    //     });
 
-        afterAll(async (done) => {
-            // delete Viewer
-            if(accViewerSub) {
-                try {
-                    await UserPool.adminDisableUser(<AdminDisableUserRequest>{
-                        Username: accViewerSub,
-                        UserPoolId: config["UserPoolId"]
-                    }).promise();
-                    await UserPool.adminDeleteUser(<AdminDeleteUserRequest>{
-                        Username: accViewerSub,
-                        UserPoolId: config["UserPoolId"]
-                    }).promise();
-                    done();
-                } catch (error) {
-                    console.log(error);
-                    done.fail(error);
-                }
-            } else {
-                done();
-            }
-        });
-    });
+    //     afterAll(async (done) => {
+    //         // delete Viewer
+    //         if(accViewerSub) {
+    //             try {
+    //                 await UserPool.adminDisableUser(<AdminDisableUserRequest>{
+    //                     Username: accViewerSub,
+    //                     UserPoolId: config["UserPoolId"]
+    //                 }).promise();
+    //                 await UserPool.adminDeleteUser(<AdminDeleteUserRequest>{
+    //                     Username: accViewerSub,
+    //                     UserPoolId: config["UserPoolId"]
+    //                 }).promise();
+    //                 done();
+    //             } catch (error) {
+    //                 console.log(error);
+    //                 done.fail(error);
+    //             }
+    //         } else {
+    //             done();
+    //         }
+    //     });
+    // });
 
-    afterAll(async (done) => {
-        // delete Account Admin
-        if(accAdmin) {
-            try {
-                await UserPool.adminDisableUser(<AdminDisableUserRequest>{
-                    Username: accAdmin.userSub,
-                    UserPoolId: config["UserPoolId"]
-                }).promise();
-                await UserPool.adminDeleteUser(<AdminDeleteUserRequest>{
-                    Username: accAdmin.userSub,
-                    UserPoolId: config["UserPoolId"]
-                }).promise();
-                done();
-            } catch (error) {
-                console.log(error);
-                done.fail(error);
-            }
-        } else {
-            done();
-        }
-    }, 120000);
+    // afterAll(async (done) => {
+    //     // delete Account Admin
+    //     if(accAdmin) {
+    //         try {
+    //             await UserPool.adminDisableUser(<AdminDisableUserRequest>{
+    //                 Username: accAdmin.userSub,
+    //                 UserPoolId: config["UserPoolId"]
+    //             }).promise();
+    //             await UserPool.adminDeleteUser(<AdminDeleteUserRequest>{
+    //                 Username: accAdmin.userSub,
+    //                 UserPoolId: config["UserPoolId"]
+    //             }).promise();
+    //             done();
+    //         } catch (error) {
+    //             console.log(error);
+    //             done.fail(error);
+    //         }
+    //     } else {
+    //         done();
+    //     }
+    // }, 120000);
 });
