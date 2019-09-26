36aff08993c2a19455647bc0cdbd2e560b48a8b3
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-26 17:08:03 +1000

    specs are now under test/specs

--- test/index.ts
+++
@@ -1,6 +0,0 @@
-import {AuthUtils} from "./auth.utils";
-(async () => {
-    await AuthUtils.setup();
-    const testsContext = require.context(".", true, /spec.*.ts/);
-    testsContext.keys().forEach(testsContext);
-})();
