4b47bb79f5274c26f34d0661d4f779b8ebcbb7fd
Kartik Rao <kartik.rao@globalred.com.au>
2019-09-26 17:04:11 +1000

    Remove src directory, moved to respective stack or client

--- src/common/Entry.ts
+++
@@ -1,4 +0,0 @@
-export type EntryMessageBodyKey      = "__RequestId"|"__RequestTimestamp"|"__RequestIpAddress"|"__RequestUserAgent"|"Payload";
-export type EntryMessageAttributeKey = "Region"|"Service"|"Stage"|"FormId"|"TenantId"|"StreamName";
-export type EntryMessageBody         = Record<EntryMessageBodyKey, string>;
-export type EntryMessageAttributeMap<T> = { [key in EntryMessageAttributeKey]?: T };
--- src/upload.ts
+++
@@ -1,3 +0,0 @@
-import _AWS from 'aws-sdk';
-import XRay from 'aws-xray-sdk';
-const AWS = XRay.captureAWS(_AWS);
