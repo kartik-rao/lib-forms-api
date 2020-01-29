46fde5cf3f128cac6193fa791435f308c05cccee
Kartik Rao <kartik.rao@globalred.com.au>
2020-01-30 10:26:45 +1100

    Get chrome via lambda layer, update runtime to 12.x

--- stacks/api-app/functions/screenshot.ts
+++ stacks/api-app/functions/screenshot.ts
@@ -4,7 +4,6 @@ process.env.TZ = 'UTC';
 
 import {APIGatewayEventRequestContext, APIGatewayEvent} from 'aws-lambda';
 import chrome from 'chrome-aws-lambda';
-import puppeteer from 'puppeteer-core';
 
 const ENV = process.env.environment;
 const ServiceName  = process.env.serviceName;
@@ -20,23 +19,24 @@ const CORS_HEADERS = {
 export const handle = async (event : APIGatewayEvent, context : APIGatewayEventRequestContext, callback : any) => {
     console.log(`${ServiceName} - screenshot.handle`, event);
     let envPrefix = ENV == "dev" ? "dev-" : ENV == "staging" ? `staging-` : "";
-    const browser = await puppeteer.launch({
-        args: chrome.args,
-        executablePath: await chrome.executablePath,
-        headless: chrome.headless
-    });
 
     try {
+        const browser = await chrome.puppeteer.launch({
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            defaultViewport : {width: 1920, height: 1080}
+        });
+
         let versionUrl = `https://${envPrefix}api.forms.li/app/view/${event.pathParameters.formId}/${event.pathParameters.versionId}`;
         const page = await browser.newPage();
+
         page.setJavaScriptEnabled(true);
-        page.setViewport({height: 1080, width: 1920});
         page.setExtraHTTPHeaders({"Authorization" : event.headers["Authorization"]});
-        page.setDefaultNavigationTimeout(10000);
+        page.setDefaultNavigationTimeout(5000);
         await page.goto(versionUrl);
         page.waitFor(5000);
-        const file = await page.screenshot({type: "png", fullPage: true});
-        callback(null, {statusCode: 200, headers: CORS_HEADERS, body: file});
+        const buffer = await page.screenshot({type: "png", fullPage: true});
+        callback(null, {statusCode: 200, headers: CORS_HEADERS, body: buffer});
     } catch (error) {
         console.error(error);
         callback(error);
